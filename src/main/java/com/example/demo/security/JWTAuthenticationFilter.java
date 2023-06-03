5bcd619537d3d6c24bce3854d34291fc0698881e
abanu <alex.banu14@gmail.com>
2023-06-03 23:06:45 +0300

    Simplify JWT authentication + authorization

--- src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
@@ -3,18 +3,14 @@ package com.example.demo.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Date;
 
 import static com.example.demo.security.SecurityConstants.EXPIRATION_TIME;
@@ -22,25 +18,11 @@ import static com.example.demo.security.SecurityConstants.SECRET;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    private final PasswordEncoder passwordEncoder;
-
-    public JWTAuthenticationFilter(AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
-        this.passwordEncoder = passwordEncoder;
 
         setUsernameParameter("email");
         setFilterProcessesUrl("/api/login");
-    }
-
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest req,
-                                                HttpServletResponse res) throws AuthenticationException {
-        return getAuthenticationManager().authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        this.obtainUsername(req),
-                        this.obtainPassword(req),
-                        new ArrayList<>())
-        );
     }
 
     @Override
--- src/main/java/com/example/demo/security/JWTAuthorizationFilter.java
+++ src/main/java/com/example/demo/security/JWTAuthorizationFilter.java
@@ -2,22 +2,25 @@ package com.example.demo.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
 
-public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+    // TODO: Remove this when retrieving users from DB
+    private final UserDetailsService userDetailsService;
 
-    public JWTAuthorizationFilter(AuthenticationManager authManager) {
-        super(authManager);
+    // TODO: Remove this when retrieving users from DB
+    public JWTAuthorizationFilter(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
     }
 
     @Override
@@ -42,15 +45,15 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
         String token = request.getHeader(SecurityConstants.HEADER_STRING);
 
         if (token != null) {
-            // parse the token.
             String user = JWT.require(Algorithm.HMAC512(SecurityConstants.SECRET.getBytes()))
                     .build()
                     .verify(token.replace(SecurityConstants.TOKEN_PREFIX, ""))
                     .getSubject();
 
             if (user != null) {
-                // new arraylist means authorities
-                return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+                // TODO: Remove this when retrieving users from DB
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(user);
+                return new UsernamePasswordAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
             }
 
             return null;
